ce35ea378ac7608f1b6e9480bbf429345fed3289
Popotojs <contact@popotojs.com>
2018-03-24 19:26:51 +0100

    keep rollup configs in npm module, add jQuery and D3 imports and first try with popoto's code

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,16 @@ import node from "rollup-plugin-node-resolve";
 export default {
     input: "index",
     plugins: [node()],
+    external: ["d3", "jquery"],
     output: {
         extend: true,
         file: "dist/popoto.js",
         format: "umd",
         indent: false,
-        name: "popoto"
+        name: "popoto",
+        globals: {
+            d3: "d3",
+            jquery: "jQuery"
+        }
     }
 };

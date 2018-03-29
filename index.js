cbf95bde442433719790266fc04ee61095b9b03d
popoto <contact@popotojs.com>
2018-03-29 14:17:22 +0200

    move css files, add factory template module, disabled tests, add css uglify

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export {default as logger} from "./src/logger/logger";
 export {default as rest} from "./src/rest/rest";
+export {default as factory} from "./src/template/factory";
---
+++ src/template/factory.js
@@ -0,0 +1,45 @@
+import {default as d3} from "d3";
+
+var factory = (function (d3) {
+    'use strict';
+
+    var self = {};
+
+    var instances = [];
+
+    self.getInstances = function () {
+        return instances;
+    };
+
+    self.create = function (containerId, config) {
+        var instance = {};
+
+        instance.margin = {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        };
+
+        instance.container = d3.select("#" + containerId);
+
+        instance.width = config.width || 600;
+        instance.height = config.height || 300;
+
+        instance.svg = instance.container.append("svg")
+            .attr("viewBox", "0 0 " + instance.width + " " + instance.height);
+
+        instance.svg.append("circle")
+            .attr("cx", 300)
+            .attr("cy", 150)
+            .attr("r", 100)
+            .style("fill", "purple");
+
+        instances.push(instance);
+        return instance;
+    };
+
+    return self;
+})(d3);
+
+export default factory;

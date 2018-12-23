4cf8f5bce25f9ff364cf47ae0701e4bac8adf0d4
Popotojs <contact@popotojs.com>
2018-12-23 15:28:57 +0100

    move predefined constrains test in dedicated generation tests

--- test/query/query.generateTaxonomyCountQuery.test.js
+++ test/query/query.generateTaxonomyCountQuery.test.js
@@ -28,3 +28,29 @@ describe("taxonomy count generation", function () {
         expect(countQuery).toMatchSnapshot();
     });
 });
+
+describe("Predefined constraints Neo4j id generation", function () {
+    beforeEach(() => {
+        provider.node.Provider = {
+            "Person": {
+                returnAttributes: ["name", "born"],
+                constraintAttribute: "name",
+                getPredefinedConstraints: function () {
+                    return ["$identifier.born > 1976"];
+                }
+            },
+            "Movie": {
+                returnAttributes: ["title", "born"],
+            }
+        };
+    });
+
+    afterEach(() => {
+        delete provider.node.Provider;
+    });
+
+    test("Person", () => {
+        var countQuery = query.generateTaxonomyCountQuery("Person");
+        expect(countQuery).toMatchSnapshot();
+    });
+});
